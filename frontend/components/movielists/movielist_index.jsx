548b8415540fb20965906ce4b2ca7cfd87b6f565
Harrison Bui <hsbui@hotmail.com>
2017-03-20 11:01:04 -0700

    change icon image

---
+++ frontend/components/movielists/movielist_index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovielistIndexItem from './movielist_index_item';
+
+
+class MovielistIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUrl: ""
+    };
+  }
+}
