8b6e8ed0d8964ca9a4941293d045c11f710a82c1
Harrison Bui <hsbui@hotmail.com>
2017-03-24 16:16:28 -0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create!(name: 'Luke', movie: movies.first)
 
-user1 = User.create!(username: 'Guest', password: 'BarryAllenGuest')
 
 movie1 = Movie.create!(title: 'Forrest Gump',
                       director: 'Robert Zemeckis',
