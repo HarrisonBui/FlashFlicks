854d7844826a141eaa0b510fcb2854dd66d63d36
Harrison Bui <hsbui@hotmail.com>
2017-04-24 22:58:14 -0700

    add star to rating

--- frontend/components/movies/movie_index_item.jsx
+++ frontend/components/movies/movie_index_item.jsx
@@ -20,21 +20,6 @@ class MovieIndexItem extends React.Component {
       removeMovielist_moviesButton = '';
     }
 
-    const fullstar = <img
-      className="icon"
-      src="http://res.cloudinary.com/dufjsfgjv/image/upload/v1490030369/star-icon_bnkv7r.png"
-      width="14"
-      height="14"
-      />;
-
-    const emptystar = <img
-      className="icon"
-      src="http://res.cloudinary.com/dufjsfgjv/image/upload/v1490030471/outline-star-xxl_jbipvt.png"
-      width="14"
-      height="14"
-      />;
-
-
     return(
       <div className='movie-index-item'  id={this.props.movie.id} key={this.props.movie.id}>
         <div className='cover'></div>
@@ -42,7 +27,13 @@ class MovieIndexItem extends React.Component {
           <img className="movie-img" src={("https://image.tmdb.org/t/p/w500" + this.props.movie.poster_path)}></img>
           <div className="show-title-rating">
             <h4>{this.props.movie.title}</h4>
-            <h4>Rating: {this.props.movie.vote_average}</h4>
+            <br></br>
+            <h4>{this.props.movie.vote_average} <img
+              className="icon"
+              src="https://img.clipartfest.com/ac53e73ecdfaddaf6d0c85cb175fe1de_big-image-png-big-yellow-star-clipart_2400-2283.png"
+              width="16"
+              height="13"
+              /> </h4>
                 {removeMovielist_moviesButton}
             </div>
         </Link>
